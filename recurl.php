5454a150a7c93e21a267199ba7280f787dd53fc5
Emmanuel Tomas Cruz <noelmcc@users.noreply.github.com>
2017-10-31 05:35:42 +0800

    Add files via upload

---
+++ recurl.php
@@ -0,0 +1,10 @@
+<?
+$recurl1 = "http://v17.mycallcloud.com";			//change this to the public address for the recording files
+$recurl2 = "http://v18.mycallcloud.com";			//change this to the public address for the recording files
+
+
+
+
+
+//add more servers as needed; use var format as shown
+?>
